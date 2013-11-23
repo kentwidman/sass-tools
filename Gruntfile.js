8333077b61b756aa7a61491cb95a3494db0a17c4
Sarah Holden <sarahbeth.holden@gmail.com>
2013-11-22 22:10:52 -0600

    update css

--- Gruntfile.js
+++ Gruntfile.js
@@ -128,7 +128,7 @@ module.exports = function (grunt) {
             },
             server: {
                 options: {
-                    debugInfo: true
+                    debugInfo: false
                 }
             }
         },
