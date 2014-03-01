878a43633b40898b13f348cdaaad28f64fc9d16d
Kent Widman <KWidman@C-K.com>
2014-03-01 15:08:05 -0600

    in the middle of refactoring.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,172 +1,113 @@
 /*global net, Modernizr */
-var app = {
-	contants: {
-		hthreshold: 0.001,
-		threshold: 0.001
-	}
-};
-var Color = net.brehaut.Color;
-
-function roundFraction(value, digits){
-	'use strict';
-	var num = Number(value);
-	return num.toFixed(digits);
-}
-
-function displayColors(color1, color2){
-	'use strict';
-
-	//console.log(color1, color2);
-	var out = color1.toCSS(),
-		amount = 0,
-		c1 = {
-			h: color1.getHue(),
-			s: color1.getSaturation(),
-			l: color1.getLightness()
+(function(){
+	var app = {
+			contants: {
+				hthreshold: 0.001,
+				threshold: 0.001
+			}
 		},
-		c2 = {
-			h: color2.getHue(),
-			s: color2.getSaturation(),
-			l: color2.getLightness()
-		},
-		diff = {
-			h: c2.h - c1.h,
-			s: c2.s - c1.s,
-			l: c2.l - c1.l
-		};
+		Color = net.brehaut.Color;
 
-	if (diff.h > app.contants.hthreshold || diff.h < -app.contants.hthreshold){
-		amount = diff.h;
-		amount = roundFraction(amount, 0);
-		out = 'adjust-hue(' + out + ', ' + amount + 'deg)';
+	//helper functions
+	function roundFraction(value, digits){
+		var num = Number(value);
+		return num.toFixed(digits);
 	}
 
-	if (diff.s > app.contants.threshold || diff.s < -app.contants.threshold){
-		if (diff.s > 0){
-			amount = (diff.s ) * 100;
-			amount = roundFraction(amount, 1);
-			out = 'saturate(' + out + ', ' + amount + '%)';
-		} else {
-			amount = (-diff.s) * 100;
-			amount = roundFraction(amount, 1);
-			out = 'desaturate(' + out + ', ' + amount + '%)';
-		}
-	}
-
-	if (diff.l > app.contants.threshold || diff.l < -app.contants.threshold){
-		if (diff.l > 0){
-			amount = (diff.l) * 100;
-			amount = roundFraction(amount, 1);
-			out = 'lighten(' + out + ', ' + amount + '%)';
-		} else {
-			amount = (-diff.l) * 100;
-			amount = roundFraction(amount, 1);
-			out = 'darken(' + out + ', ' + amount + '%)';
-		}
-	}
-
-	out += ';';
-	return out;
-}
-
-function stringToArray(str){
-	'use strict';
-
-	var pointsStr = str.split(' ');
-	var points = [],
+	function stringToArray(str){
+		var pointsStr = str.split(' ');
+		var points = [],
 			i,
 			j;
 
-	for (i=0; i < pointsStr.length; i++){
-		points.push(pointsStr[i].split(','));
-	}
-
-	for (i=0; i < points.length; i++){
-		for (j=0; j < 2; j++){
-			points[i][j] = parseInt(points[i][j], 10);
+		for (i=0; i < pointsStr.length; i++){
+			points.push(pointsStr[i].split(','));
 		}
-	}
-	return points;
-}
 
-
-function arrayToString(arr){
-	'use strict';
-
-	var pointsStr = '';
-
-	for (var i=0; i < arr.length; i++) {
-		pointsStr += arr[i][0].toString() + ',' + arr[i][1].toString() + ' ';
-	}
-	return pointsStr;
-}
-
-function cleanStr(arr){
-	'use strict';
-
-	var out = arr;
-	out = out.replace(/^\s+|\s+$/g, '');
-	out = out.replace('#','');
-	out = out.toLowerCase();
-	return out;
-}
-
-function isValidColor(color){
-	'use strict';
-	return (/^[a-f0-9]{3}$|^[a-f0-9]{6}$/i).test(color);
-}
-
-/* resize */
-$(document).ready(function() {
-	'use strict';
-
-	$('#about').fitText(1, { minFontSize: '25px', maxFontSize: '95px' });
-
-	var $window = $(window),
-		colors = {},
-		tri1 = $('#tri1'),
-		tri2 = $('#tri2'),
-		tri1PointsStr = tri1.attr('points'),
-		tri2PointsStr = tri2.attr('points'),
-		//tri1Color = tri1.attr('fill'),
-		//tri2Color = tri2.attr('fill'),
-		tri1Points = stringToArray(tri1PointsStr),
-		tri2Points = stringToArray(tri2PointsStr),
-		tri1DefColor = 'F26531',
-		tri2DefColor = '1A1A1A';
-
-
-	$('#color1, #color2').on('input', function(){
-		var $this = $(this),
-			id = $this.attr('id'),
-			val = cleanStr($this.val());
-
-		if (isValidColor(val)){
-			colors[id] = new Color('#'+cleanStr(val));
-			if (id === 'color1') {
-				tri1.attr('fill', '#'+val);
-			} else if (id === 'color2') {
-				tri2.attr('fill', '#'+val);
+		for (i=0; i < points.length; i++){
+			for (j=0; j < 2; j++){
+				points[i][j] = parseInt(points[i][j], 10);
 			}
-		} else {
-			delete colors[id];
-			if (id === 'color1') {
-				tri1.attr('fill', '#'+tri1DefColor);
-			} else if (id === 'color2') {
-				tri2.attr('fill', '#'+tri2DefColor);
+		}
+		return points;
+	}
+
+	function arrayToString(arr){
+		var pointsStr = '';
+
+		for (var i=0; i < arr.length; i++) {
+			pointsStr += arr[i][0].toString() + ',' + arr[i][1].toString() + ' ';
+		}
+		return pointsStr;
+	}
+
+	function cleanStr(arr){
+		var out = arr;
+		out = out.replace(/^\s+|\s+$/g, '');
+		out = out.replace('#','');
+		out = out.toLowerCase();
+		return out;
+	}
+
+	function isValidColor(color){
+		return (/^[a-f0-9]{3}$|^[a-f0-9]{6}$/i).test(color);
+	}
+
+	function displayColors(color1, color2){
+		//console.log(color1, color2);
+		var out = color1.toCSS(),
+			amount = 0,
+			c1 = {
+				h: color1.getHue(),
+				s: color1.getSaturation(),
+				l: color1.getLightness()
+			},
+			c2 = {
+				h: color2.getHue(),
+				s: color2.getSaturation(),
+				l: color2.getLightness()
+			},
+			diff = {
+				h: c2.h - c1.h,
+				s: c2.s - c1.s,
+				l: c2.l - c1.l
+			};
+
+		if (diff.h > app.contants.hthreshold || diff.h < -app.contants.hthreshold){
+			amount = diff.h;
+			amount = roundFraction(amount, 0);
+			out = 'adjust-hue(' + out + ', ' + amount + 'deg)';
+		}
+
+		if (diff.s > app.contants.threshold || diff.s < -app.contants.threshold){
+			if (diff.s > 0){
+				amount = (diff.s ) * 100;
+				amount = roundFraction(amount, 1);
+				out = 'saturate(' + out + ', ' + amount + '%)';
+			} else {
+				amount = (-diff.s) * 100;
+				amount = roundFraction(amount, 1);
+				out = 'desaturate(' + out + ', ' + amount + '%)';
 			}
 		}
 
-		if (colors.color1 && colors.color2) {
-			$('#sass-output').html(displayColors(colors.color1, colors.color2));
-		} else {
-			$('#sass-output').html('');
+		if (diff.l > app.contants.threshold || diff.l < -app.contants.threshold){
+			if (diff.l > 0){
+				amount = (diff.l) * 100;
+				amount = roundFraction(amount, 1);
+				out = 'lighten(' + out + ', ' + amount + '%)';
+			} else {
+				amount = (-diff.l) * 100;
+				amount = roundFraction(amount, 1);
+				out = 'darken(' + out + ', ' + amount + '%)';
+			}
 		}
-	});
 
+		out += ';';
+		return out;
+	}
 
-	$(window).bind('resize', function(){
-
+	function resizeSvg(){
 		var h = $window.height(),
 			w = $window.width();
 
@@ -222,12 +163,62 @@ $(document).ready(function() {
 
 		tri1.attr('points', tri1String);
 		tri2.attr('points', tri2String);
+	}
 
-	}).trigger('resize');
+	/* resize */
+	$(document).ready(function() {
+		'use strict';
 
-});
+		$('#about').fitText(1, { minFontSize: '25px', maxFontSize: '95px' });
+
+		var $window = $(window),
+			colors = {},
+			$tris = [$('#tri1'), $('#tri2')],
+			triPointsStr = [tri1.attr('points'), tri2.attr('points')];
+
+		triPoints = [stringToArray(triPointsStr[0]), stringToArray(stringToArray(triPointsStr[0])];
+		tri1Points = stringToArray(tri1PointsStr);
+		tri2Points = stringToArray(tri2PointsStr);
+		tri1DefColor = 'F26531';
+		tri2DefColor = '1A1A1A';
 
 
+		$('#color1, #color2').on('input', function(){
+			var $this = $(this),
+				validIds = ['color1','color2'],
+				id = $this.attr('id'),
+				val = cleanStr($this.val());
+
+			//check to make sure input is a valid color.
+			if (isValidColor(val)){
+				//update color
+				colors[id] = new Color('#'+cleanStr(val));
+				for (var i = 0; i < $tris.length; i++) {
+					if (id === validIds[i]) {
+						$tris[i].attr('fill', '#'+val);
+						break;
+					}
+				}
+			} else {
+				//revert to default color
+				delete colors[id];
+				for (var i = 0; i < $tris.length; i++){
+					if (id === validIds[i]) {
+						$tris[i].attr('fill', '#'+tri1DefColor);
+						break;
+					}
+				}
+			}
+
+			if (colors.color1 && colors.color2) {
+				$('#sass-output').html(displayColors(colors.color1, colors.color2));
+			} else {
+				$('#sass-output').html('');
+			}
+		});
 
 
+		$(window).bind('resize', resizeSvg).trigger('resize');
 
+	});
+})();
